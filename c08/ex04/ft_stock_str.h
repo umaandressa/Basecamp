1d234460b967b568acdba58941e8fd764f58aaf6
ade-souz <ade-souz@student.42sp.org.br>
2021-04-16 23:20:06 +0000

    correct c04.ex08

---
+++ c08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-souz <ade-souz@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 19:52:37 by ade-souz          #+#    #+#             */
+/*   Updated: 2021/04/15 23:51:03 by ade-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct	s_stock_str
+{
+	int size;
+	char *str;
+	char *copy;
+}				t_stock_str;
+
+#endif
